3265edfd199fddf53abec5bcc16ffc428e711362
Seval Künler <knlr.swl@hotmail.com>
2022-08-29 17:53:49 +0300

    localStorage

--- script.js
+++ script.js
@@ -7,14 +7,16 @@ const text = document.getElementById('text');
 const amount = document.getElementById('amount');
 
 
-const dummyTransactions=[
-    {id:1, text:'Flower', amount:-20},
-    {id:2, text:'Salary', amount:300},
-    {id:3, text:'Book', amount:-10},
-    {id:4, text:'Camera', amount:150},
-];
+// const dummyTransactions=[
+//     {id:1, text:'Flower', amount:-20},
+//     {id:2, text:'Salary', amount:300},
+//     {id:3, text:'Book', amount:-10},
+//     {id:4, text:'Camera', amount:150},
+// ];
 
-let transactions =dummyTransactions;
+const localStorageTransactions =JSON.parse(localStorage.getItem('transactions'));
+
+let transactions =localStorage.getItem('transactions') !== null ? localStorageTransactions : [];
 
 function addTransaction(e){
     e.preventDefault();
@@ -31,6 +33,8 @@ function addTransaction(e){
         transactions.push(transaction);
         addTransactionDOM(transaction)
         updateValues();
+
+        updateLocalStorage();
 
         text.value='';
         amount.value=''
@@ -73,7 +77,12 @@ function updateValues(){
 function removeTransaction(id){
     transactions= transactions.filter(transaction => transaction.id !== id);
 
+    updateLocalStorage();
     init ();
+}
+
+function updateLocalStorage(){
+    localStorage.setItem('transactions', JSON.stringify(transactions))
 }
 
 function init(){
